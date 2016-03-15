b13f71fc0c236e562b9b61cf4d367ffa1c7b1e72
hartsantler <goatman.py@gmail.com>
2016-03-15 08:09:46 -0700

    testing returning `self` (translated into c++ `auto`) from subclasses works ok. TODO, fix subclass needs to redefine methods that return `self`, because the base class returns self, and that demotes the subclass when calling that method returning self.

--- regtests/c++/returns_auto.py
+++ regtests/c++/returns_auto.py
@@ -23,12 +23,32 @@ def returns_float():
 	return 1.1
 
 class Foo:
+	def __init__(self, bar:int):
+		self.bar = bar
 	def get_int(self):
 		return 3
 	def get_float(self):
 		return 4.4
 	def do_nothing(self):
 		pass
+	def get_self(self):
+		return self
+
+with stack:
+	class FooStack:
+		def __init__(self, bar:int):
+			self.bar = bar
+		def get_self(self):
+			return self
+
+	class FooStackSub( FooStack ):
+		def __init__(self, bar:int, value:float):
+			self.bar = bar
+			self.value = value
+		## TODO: subclasses should automatically generate this
+		def get_self(self):
+			return self
+
 
 def main():
 	print returns_int( False )
@@ -36,10 +56,29 @@ def main():
 	assert returns_int( True )==1
 	assert returns_float() == 1.1
 
-	f = Foo()
+	f = Foo(400)
 	assert f.get_int()==3
 	assert f.get_float()==4.4
 	f.do_nothing()
+	assert f.bar==400
+	assert f.get_self().bar==400
+
+	with stack:
+		sf = FooStack(100)
+		assert sf.bar==100
+		assert sf.get_self().bar==100
+
+		subf = FooStackSub(10, 1.1)
+		print subf.bar
+		print subf.value
+		assert subf.bar == 10
+		#assert subf.value == 1.1
+		o = subf.get_self()
+		assert o.bar == 10
+		#assert o.value == 1.1
+
+		print o.bar
+		print o.value
 
 	print('OK')
 
